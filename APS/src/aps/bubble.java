8d7fc01fd6a13237bbc28ce88de9fead14dd5a91
Leonardo Ribeiro Leonardi <49618344+leonardo99i@users.noreply.github.com>
2021-11-23 19:08:11 -0300

    BubbleSort Funcionado

---
+++ APS/src/aps/bubble.java
@@ -0,0 +1,43 @@
+package aps;
+
+public class bubble{
+
+    public static int contBubble = 0; 
+    public static void main(String[] args) {
+        int[] vetor = new int[10];
+        for (int i = 0; i < vetor.length; i++) {
+            vetor[i] = (int) Math.floor(Math.random() * 100);
+        }
+
+        System.out.println("Desordenado");
+        for (int i = 0; i < vetor.length; i++) {
+            System.out.print(vetor[i] + " ");
+        }
+        System.out.println();
+
+        bubbleSort(vetor, vetor.length);
+
+        System.out.println("Vetor Ordenado com BubbleSort:");
+        for (int i = 0; i < vetor.length; i++) {
+            System.out.print(vetor[i] + " ");
+        }
+        System.out.println();
+        System.out.println("Contador: " + contBubble);
+        
+    }
+
+    public static int bubbleSort(int[] vetor, int length){
+        int aux;
+        for(int i = 0; i < vetor.length; i++){
+            for(int j = 0; j < vetor.length - 1; j++){
+                if(vetor[j] < vetor[j + 1]){
+                    aux = vetor[j];
+                    vetor[j] = vetor[j+1];
+                    vetor[j+1] = aux;
+                    contBubble++;
+                }
+            }
+        }
+        return contBubble;
+    }
+}
