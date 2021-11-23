9ea1f66b8bb44626458e8e9b7047db2247473f7d
Leonardo Ribeiro Leonardi <49618344+leonardo99i@users.noreply.github.com>
2021-11-23 19:29:05 -0300

    InsertionSort funcionado

---
+++ APS/src/aps/insetion.java
@@ -0,0 +1,43 @@
+package aps;
+
+public class insetion {
+
+    public static int contInsertion = 0;
+
+    public static void main(String[] args) {
+        int[] vetor = new int[5];
+        for (int i = 0; i < vetor.length; i++) {
+            vetor[i] = (int) Math.floor(Math.random() * 100);
+        }
+
+        System.out.println("Desordenado:");
+        for (int i = 0; i < vetor.length; i++) {
+            System.out.print(vetor[i] + " ");
+        }
+        System.out.println();
+
+        insertionSort(vetor, vetor.length);
+
+        System.out.println("Vetor Ordenado com InsertionSort:");
+        for (int i = 0; i < vetor.length; i++) {
+            System.out.print(vetor[i] + " ");
+        }
+        System.out.println();
+        System.out.println("Contador: " + contInsertion);
+    }
+
+    public static int insertionSort(int[] vetor, int length) {
+        int a, j;
+        for(int i = 1; i < vetor.length; i++){
+            a = vetor[i];
+            j = i - 1;
+            contInsertion++;
+            while((j >= 0) && vetor[j] < a){
+                vetor[j + 1] = vetor[j];
+                j= j - 1;
+            }
+            vetor[j+1] = a;
+        }
+        return contInsertion;
+    }
+}
